c53012cf4ee12c80f669884fb716f00ebb684aa3
getsometoast <james@jameslewiscv.com>
2013-05-06 18:16:50 +0100

    add the elasticsearch part of the code add a little test data

--- MemcachedTest/Program.cs
+++ MemcachedTest/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Nest;
+using Newtonsoft.Json;
 
 namespace MemcachedTest
 {
@@ -11,12 +14,55 @@ namespace MemcachedTest
         {
             var elasticsearchConnection = "http://192.168.56.1:9200/";
             var memcachedConnection = "http://10.211.55.10:11211/";
-            var rabbitMqConnection = "http://127.0.0.1:54325/";
+            //var rabbitMqConnection = "http://127.0.0.1:54325/";
 
             var blah = new Enyim.Caching.Configuration.MemcachedClientConfiguration();
             blah.AddServer(memcachedConnection);
 
-            
+            // setup memcached
+            // setup elasticsearch
+            var connection = new ConnectionSettings(new Uri(elasticsearchConnection));
+            var client = new ElasticClient(connection);
+
+            // read the data
+            var jsonData = File.ReadAllText("Data/test-data.json");
+            var data = JsonConvert.DeserializeObject<Data>(jsonData);
+
+            // check the alias exists in memcached
+            var aliasExists = false;
+            AliasParams alias = null;
+
+            // if not:
+            if (!aliasExists)
+            {
+                
+                // create the alias
+                alias = new AliasParams
+                {
+                    Alias = data.UserId.ToString(),
+                    Filter = data.UserId.ToString(),
+                    Index = "the-index",
+                    Routing = data.UserId.ToString()
+                };
+
+                client.Alias(alias);
+
+                // add the alias to memcached
+
+
+            }
+
+            // insert data into elasticsearch via the alias
+            client.Index<Data>(data, new IndexParameters { Routing = alias.Routing }); // against the alias - how in NEST?!!
         }
+    }
+
+    // some class representing the model
+    private class Data
+    {
+        public string Id { get; set; }
+        public int UserId { get; set; }
+        public int TrackId { get; set; }
+        public string Blah { get; set; }
     }
 }
