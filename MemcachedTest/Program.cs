4cb12c0234f58419ecbf62cd9a063815e06b2d0a
getsometoast <james@jameslewiscv.com>
2013-05-06 17:55:16 +0100

    initial commit to add the project and solution files

---
+++ MemcachedTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MemcachedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elasticsearchConnection = "http://192.168.56.1:9200/";
+            var memcachedConnection = "http://10.211.55.10:11211/";
+            var rabbitMqConnection = "http://127.0.0.1:54325/";
+
+            var blah = new Enyim.Caching.Configuration.MemcachedClientConfiguration();
+            blah.AddServer(memcachedConnection);
+
+            
+        }
+    }
+}
